06c9bd36b00cf59cc21a016a444e2db080291643
Pratham Solanki <pratham.c.solanki@gmail.com>
2017-03-04 20:22:28 +0530

    initial commit

---
+++ Union-Find/QuickUnionUF.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+class QuickUnionUF
+{
+	private:
+		int *id = NULL;
+
+		int root(int i)
+		{
+			while(i != id[i])
+				i = id[i];
+			return i;
+		}
+
+	public:
+		QuickUnionUF(int N)
+		{	
+			id = new int[N];
+			for(int i = 0; i < N; i++)
+				id[i] = i;
+		}
+
+		bool isConnected(int p, int q)
+		{
+			if (p == q) {
+				return true;
+			} else {
+				return root(p)==root(q);
+			}
+		}
+
+		void doUnion(int p, int q)
+		{
+			if (p == q) {
+				return;
+			} else {
+				int rootp = root(p);
+				int rootq = root(q);
+				id[rootp] = rootq;	
+			}
+		}	
+};
+
+int main()
+{
+	int N;
+	cout << "Enter number of components: ";
+	cin >> N; 	
+	QuickUnionUF UnionFind(N);
+	UnionFind.doUnion(1,3);
+	cout << UnionFind.isConnected(1,3) << endl;
+	return 0;
+}
---
+++ Union-Find/WeightedQuickUnionUF.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+using namespace std;
+
+class WeightedQuickUnionUF 
+{
+	private:
+		int *id = NULL;
+		int *size = NULL;
+
+		int root(int i)
+		{
+			while (i != id[i]) {
+				i = id[i];
+				id[i] = id[id[i]];
+			}
+			return i;
+		}
+
+	public:
+		WeightedQuickUnionUF(int N)
+		{	
+			id = new int[N];
+			size = new int[N];
+			for (int i = 0; i < N; i++) {
+				id[i] = i;
+				size[i] = 1;
+			}
+		}
+
+		bool isConnected(int p, int q)
+		{
+			if (p == q) {
+				return true;
+			} else {
+				return root(p)==root(q);
+			}
+		}
+
+		void doUnion(int p, int q)
+		{
+			int rootp = root(p);
+			int rootq = root(q);
+
+			if (size[rootp] < size[rootq]) {
+				id[rootp] = rootq;
+				size[rootq] += size[rootp];
+			} else {
+				id[rootq] = rootp;
+				size[rootp] += size[rootq];				
+			}	
+		}	
+};
+
+int main()
+{
+	int N;
+	cout << "Enter number of components: ";
+	cin >> N; 	
+	WeightedQuickUnionUF UnionFind(N);
+	UnionFind.doUnion(1,3);
+	cout << UnionFind.isConnected(1,3) << endl;
+	return 0;
+}
