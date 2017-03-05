43f18c6fe5d9c0c1c1626b57c91c217dc677cb44
Pratham Solanki <pratham.c.solanki@gmail.com>
2017-03-06 03:02:30 +0530

    SinglyLinkedList Insertions done

--- Linked Lists/SinglyLinkedList.cpp
+++ Linked Lists/SinglyLinkedList.cpp
@@ -10,29 +10,13 @@ struct Node
 
 class SinglyLinkedList
 {
-	public:
-		Node *start;		
+	private:
+		Node *start;
 
+	public:
 		SinglyLinkedList ()
 		{
 			start = NULL;
-		}
-
-		void display ()
-		{
-			if (start == NULL) {
-				cout << "The Linked List is empty." << endl;
-				return;
-			}
-		
-			Node *temp;
-			temp = start;
-
-			while (temp != NULL) {
-				cout << temp->data << "->";
-				temp = temp->next;
-			}
-			cout << "NULL" << endl;
 		}
 
 		Node* CreateNode (int value)		
@@ -50,6 +34,41 @@ class SinglyLinkedList
 				node->next = NULL;
 				return node;
 			}
+		}
+
+		int size ()
+		{
+			if (start == NULL) {
+				cout << "The Linked List is empty." << endl;
+				return 0;
+			}
+
+			int count = 0;		
+			Node *temp;
+			temp = start;
+
+			while (temp != NULL) {
+				count++;
+				temp = temp->next;
+			}
+			return count;
+		}
+
+		void display ()
+		{
+			if (start == NULL) {
+				cout << "The Linked List is empty." << endl;
+				return;
+			}
+		
+			Node *temp;
+			temp = start;
+
+			while (temp != NULL) {
+				cout << temp->data << "->";
+				temp = temp->next;
+			}
+			cout << "NULL" << endl;
 		}
 
 		void InsertAtFirst (int value)
@@ -84,6 +103,30 @@ class SinglyLinkedList
 			OldLast->next = NewLast;
 			NewLast->next = NULL;
 		}
+
+		void InsertAtPos (int pos, int value)
+		{
+			if (pos > 1  && pos <= size()) {
+				Node *NewNode, *TraversalNode, *ptr;
+
+				NewNode = CreateNode(value);
+				TraversalNode = start;
+
+				for (int i = 1; i < pos; i++) {
+					ptr = TraversalNode;
+					TraversalNode = TraversalNode->next;
+				}
+
+				ptr->next = NewNode;
+				NewNode->next = TraversalNode;
+			}
+
+			else if (pos == 1)
+				InsertAtFirst(value);
+
+			else
+				cout << "Insert Position out of range." << endl;
+		}
 };
 
 int main()
@@ -93,6 +136,10 @@ int main()
 	obj.display();
 	obj.InsertAtFirst(4);
 	obj.InsertAtLast(4);
+	obj.display();
+	obj.InsertAtPos(4,4);
+	obj.InsertAtPos(1,4);
+	obj.InsertAtPos(2,2);
 	obj.display();
 	return 0;
 }
