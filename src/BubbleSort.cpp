4e434e60bafb8799a4d8d7d2f1f21093b2489154
Pratham Solanki <pratham.c.solanki@gmail.com>
2017-03-11 16:36:25 +0530

    BubbleSort improvised

--- src/BubbleSort.cpp
+++ src/BubbleSort.cpp
@@ -10,9 +10,16 @@ void BubbleSort::swap (int *Array, int a, int b)
 BubbleSort::BubbleSort (int *Array, int N)
 {
 	for (int k = 1; k < N; k++) {
-		for (int i = 0; i < N-1; i++) {
-			if (Array[i] > Array[i+1])
+		int flag = 0;
+
+		for (int i = 0; i < N-k; i++) {
+			if (Array[i] > Array[i+1]) {
 				swap(Array,i,i+1);
+				flag = 1;
+			}
 		}
+
+		if (flag == 0)
+			break;
 	}	
 }
